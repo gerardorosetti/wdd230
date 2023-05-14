cb183925c0b36ed2b92ccb4d12851cb0f21b25db
Saymed <gasayagom@gmail.com>
2023-05-13 21:16:43 -0400

    Home Chamber of commer done

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,45 @@
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+function togglemenu() {
+  document
+    .getElementsByClassName('navigation')[0]
+    .classList.toggle('responsive');
+}
+function togglemenu2() {
+  document.getElementsByClassName('navigation2')[0]
+  .classList.toggle('responsive2');
+}
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+const dayName = daynames[currentDate.getDay()]
+const monthName = months[currentDate.getMonth()]
+
+document.querySelector('.footer-year').textContent = currentYear;
+
+document.querySelector('.last-updated').textContent = `Last Updated: ${document.lastModified}`
+
+document.querySelector('.date-header').textContent = `${dayName}, ${currentDate.getDate()} ${monthName} ${currentYear}`
