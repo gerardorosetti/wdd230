b2c69fc27db863f6b874944628e6935824fcbc01
Saymed <gasayagom@gmail.com>
2023-06-01 23:55:18 -0400

    progressive loading images

--- lesson-7/scripts/script.js
+++ lesson-7/scripts/script.js
@@ -1,50 +1,66 @@
-fetch("https://api.pexels.com/v1/search?query=people",{
-  headers: {
-    Authorization: "GYh2Cm1DMrNRHwbKldbixrEAwxk5VGGHAmqXJDp06lnwQE8sOvQqjmSR"
-  }
-})
-   .then(resp => {
-     return resp.json()
-   })
-   .then(data => {
-     console.log(data)
-   })
-
 let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) =>
-{
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () =>
-    {
-        image.removeAttribute("data-src");
-    };
-};
 
-imagesToLoad.forEach((img) =>
+async function getImages()
 {
-    loadImages(img);
-});
+    let url = "https://api.pexels.com/v1/search?query=universe"
 
-if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, observer) =>
+    let res = await fetch(url,{
+            headers:{
+                Authorization: "GYh2Cm1DMrNRHwbKldbixrEAwxk5VGGHAmqXJDp06lnwQE8sOvQqjmSR"
+            }});
+    let data = await res.json();
+    let images = [];
+    let i = 0
+    for(i=0; i < 6 ;i++)
     {
-        items.forEach((item) =>
+        images.push(data['photos'][i]['src']['medium'])
+    }
+    i = 0
+    imagesToLoad.forEach((img) =>
+    {
+        img.setAttribute("data-src", images[i]);
+        i++;
+    });
+}
+
+getImages()
+
+setTimeout(function(){
+    const loadImages = (image) =>
+    {
+        if (image.getAttribute('data-src') != 'images/placeholder.jpg')
         {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
+            image.setAttribute("src", image.getAttribute("data-src"));
+            image.onload = () =>
+            {
+                image.removeAttribute("data-src");
+            };
+        }
+    };
+    if ("IntersectionObserver" in window) {
+        const observer = new IntersectionObserver((items, observer) =>
+        {
+            items.forEach((item) =>
+            {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    if (item.target.getAttribute('data-src') != 'images/placeholder.jpg')
+                    {
+                        observer.unobserve(item.target);
+                    }
+                }
+            });
         });
-    });
-    imagesToLoad.forEach((img) =>
+        imagesToLoad.forEach((img) =>
+        {
+            observer.observe(img);
+        });
+    }
+    else
     {
-        observer.observe(img);
-    });
-}
-else
-{
-    imagesToLoad.forEach((img) =>
-    {
-        loadImages(img);
-    });
-}
+        imagesToLoad.forEach((img) =>
+        {
+            loadImages(img);
+        });
+    }
+}, 50);
