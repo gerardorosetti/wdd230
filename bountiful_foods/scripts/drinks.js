3732d6c0555487365eb523bbb1927234ed5899a2
Saymed <gasayagom@gmail.com>
2023-07-15 00:15:46 -0400

    adding some information

--- bountiful_foods/scripts/drinks.js
+++ bountiful_foods/scripts/drinks.js
@@ -16,5 +16,5 @@ if (drinks_submitted){
     if (localStorage.getItem("drinks") == 0)
 	    drinks_submitted.innerHTML = "You have not submitted any drink, Go to the Fresh section and do it!!! You will not regret it!!!";
     else
-        drinks_submitted.innerHTML = `You have submitted ${localStorage.getItem("drinks")}!!! Go and enjoy ordering more fruit drinks in the Fresh section!!!`;
+        drinks_submitted.innerHTML = `You have submitted ${localStorage.getItem("drinks")} specialty drinks!!! Go and enjoy ordering more fruit drinks in the Fresh section!!!`;
 }
